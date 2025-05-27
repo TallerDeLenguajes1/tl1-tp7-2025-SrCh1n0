7a759228b9b320b96dc95e4156c843847a2ba54c
SrCh1n0 <facundo.lopez_hd@hotmail.com>
2025-05-27 16:00:38 -0300

    Ejercicio 1 Completo

---
+++ CALCULADORA/Program.cs
@@ -0,0 +1,48 @@
+﻿using EspacioCalculadora;
+
+Calculadora Calc = new Calculadora();
+Console.WriteLine(Calc.Resultado.ToString());
+Calc.Sumar(10);
+Console.WriteLine(Calc.Resultado.ToString());
+
+Console.WriteLine("Ingrese una opcion: \n" +
+                    "0: Mostrar Resultado \n" +
+                    "1: Sumar \n" +
+                    "2: Restar \n" +
+                    "3: Multiplicar \n" +
+                    "4: Dividir \n" +
+                    "5: Limpiar \n" +
+                    "6: Salir");
+int opcion = Console.Read();
+int valor;
+switch (opcion)
+{
+    case 0:
+        Console.WriteLine(Calc.Resultado.ToString());
+        break;
+    case 1:
+        Console.WriteLine("Ingrese un numero para sumar: ");
+        valor = Console.Read();
+        Calc.Sumar(valor);
+        break;
+    case 2:
+        Console.WriteLine("Ingrese un numero para restar: ");
+        valor = Console.Read();
+        Calc.Restar(valor);
+        break;
+    case 3:
+        Console.WriteLine("Ingrese un numero para multiplicar: ");
+        valor = Console.Read();
+        Calc.Multiplicar(valor);
+        break;
+    case 4:
+        Console.WriteLine("Ingrese un numero para dividir: ");
+        valor = Console.Read();
+        Calc.Dividir(valor);
+        break;
+    case 5:
+        Calc.Limpiar();
+        break;
+    default:
+        break;
+}
---
+++ CALCULADORA/calculadora.cs
@@ -0,0 +1,36 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    private double resultado;
+
+    public double Resultado { get => resultado; }
+
+    public void Sumar(double termino)
+    {
+        resultado += termino;
+    }
+
+    public void Restar(double termino)
+    {
+        resultado -= termino;
+    }
+
+    public void Multiplicar(double termino)
+    {
+        resultado = resultado * termino;
+    }
+
+    public void Dividir(double termino)
+    {
+        if (termino != 0)
+        {
+            resultado = resultado / termino;
+        }
+    }
+
+    public void Limpiar()
+    {
+        resultado = 0;
+    }
+}
